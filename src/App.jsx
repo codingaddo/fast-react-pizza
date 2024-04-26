f84c79d2afe36cf6a0911d5d378be1fadca5ec07
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2024-04-26 03:53:46 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/menu',
+    element:<Menu/>
+  },
+  {
+    path:'/cart',
+    element:<Cart/>
+  },
+  {
+    path:'/order/new',
+    element:<CreateOrder/>
+  },
+  {
+    path:'/order/:orderId',
+    element:<Order/>
+  }
+])
 
 const App = () => {
   
   return (
-    <div>hello</div>
+    <RouterProvider router={router}/>
   )
 }
 
