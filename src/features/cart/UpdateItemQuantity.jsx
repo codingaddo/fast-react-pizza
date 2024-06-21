a01c54b23662846f4408a9daf3a0b795cfd15071
Addo Michael <michaeladdo1997@gmail.com>
2024-06-21 12:12:51 +0000

    .

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import Button from '../../ui/Button'
-import { useDispatch, useSelector } from 'react-redux'
-import { decreaseItemQuantity, getCurrentQuantityById, increaseItemQuantity } from './cartSlice'
+import React from "react";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
 
-const UpdateItemQuantity = ({pizzaId,currentQuantity}) => {
-    const dispatch = useDispatch()
-//   const currentQuantity = useSelector(getCurrentQuantityById(id))
+const UpdateItemQuantity = ({ pizzaId, currentQuantity }) => {
+  const dispatch = useDispatch();
+  //   const currentQuantity = useSelector(getCurrentQuantityById(id))
 
   return (
-    <div className='flex items-center gap-3'>
-        <Button type={'round'} onclick={()=>{dispatch(decreaseItemQuantity(pizzaId))}}>-</Button>
-        <span className='text-sm font-medium'>{currentQuantity}</span>
-        <Button type={'round'} onclick={()=>{dispatch(increaseItemQuantity(pizzaId))}}>+</Button>
+    <div className="flex items-center gap-3">
+      <Button
+        type={"round"}
+        onclick={() => {
+          dispatch(decreaseItemQuantity(pizzaId));
+        }}
+      >
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button
+        type={"round"}
+        onclick={() => {
+          dispatch(increaseItemQuantity(pizzaId));
+        }}
+      >
+        +
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default UpdateItemQuantity
+export default UpdateItemQuantity;
