d4ef0592b5300b88fb2e0f4a66f26b7779d946c5
Addo Michael <michaeladdo1997@gmail.com>
2024-06-21 12:37:24 +0000

    .

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import UserName from "../features/user/UserName";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const username = useSelector((state) => state.user.userName);
   return (
     <header className="bg-yellow-500 flex items-center justify-between  uppercase px-4 py-3 border-b border-stone-200">
       <Link to={"/"} className=" tracking-widest">
         Fast Pizza.Co
       </Link>
       <SearchOrder />
-      <UserName />
+      {/* <UserName /> */}
+      {!username ? "" : <div className="text-sm font-semibold">{username}</div>}
     </header>
   );
 };
