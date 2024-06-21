ea5e125d51cec6ba05da5ba6d2488a495fe7cc94
Addo Michael <michaeladdo1997@gmail.com>
2024-06-21 12:28:34 +0000

    .

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SearchOrder from '../features/order/SearchOrder'
-import UserName from '../features/user/userName'
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 const Header = () => {
   return (
-    <header className='bg-yellow-500 flex items-center justify-between  uppercase px-4 py-3 border-b border-stone-200'>
-        <Link to={'/'} className=' tracking-widest'>Fast Pizza.Co</Link>
-        <SearchOrder/>
-        <UserName/>
-        
+    <header className="bg-yellow-500 flex items-center justify-between  uppercase px-4 py-3 border-b border-stone-200">
+      <Link to={"/"} className=" tracking-widest">
+        Fast Pizza.Co
+      </Link>
+      <SearchOrder />
+      <UserName />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
